9d57c05042f3b79c00c6020ac31f856ad52d1222
dhdhsm <2323224ss@163.com>
2021-04-17 20:46:26 +0800

    my

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 var x;
 var y="SIXTH";
 var z="Hint : 6 is A number";
+var m;
+var n;
+var questions =["What letter is a question?","What letter is a animal?","Which letter is a drink?",
+"What 5-letter word has 6 left when you take 2 letters away?","Which letter is around an island?",
+"What letter is a vegetable? ","How many letters are in the alphabet?"];
+var answers=["Y","B","T","SIXTH","C","P","EIGHT"];
+var hints=["Hint: the letter is like a tree branch.","Hint:Bee","Hint:People drink it in afternoon.",
+"Hint : 6 is A number","Hint:See","Hint:Pea","Hint:'alphabet' is a word."];
+
 function my_func(){
         x=document.getElementById("ans").value;
         if(x==y) {alert("Sucessful!") ;
@@ -15,8 +24,14 @@ function tip(){
 }
 
 function re(){
-
-    document.getElementById("question").innerHTML = "What letter is a question?";
-    y="Y";
-    z="Hint: the letter is like a tree branch.";
+    n=m;
+    m=parseInt(6*Math.random());
+    if (m==n & m<=6){
+      m=m+1;
+    }
+    else {m=m;}
+    console.log(m);
+    document.getElementById("question").innerHTML = questions[m];
+    y=answers[m];
+    z=hints[m];
 }
